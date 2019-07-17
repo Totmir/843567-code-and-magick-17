cb5d3481767e8960e28b70281530f5d1924ce75d
totmir <alex.khokhlov1@ya.ru>
2019-07-17 17:53:19 +0300

    added the surnames

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,5 @@
 var names = ['Иван', 'Хуан' + 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да' + 'Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -46,7 +47,7 @@ var characters = [
 var renderWizard = function(wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = randomArrayValue(names);
+  wizardElement.querySelector('.setup-similar-label').textContent = randomArrayValue(names) + randomArrayValue(surnames);
   wizardElement.querySelector('.wizard-coat').setAttribute('fill', randomArrayValue(coatColors));
   wizardElement.querySelector('.wizard-eyes').setAttribute('fill', randomArrayValue(eyesColors));
 
