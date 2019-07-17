aa572b2b3c6cf802288657f51c81e96d128d1062
totmir <alex.khokhlov1@ya.ru>
2019-07-17 17:07:58 +0300

    done with 1st part

--- js/setup.js
+++ js/setup.js
@@ -1,39 +1,63 @@
-var setup = document.querySelectorAll('.setup');
-setup.classList.remove('hidden');
-
-
-
 var names = ['Иван', 'Хуан' + 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var setupSelector = document.querySelectorAll('.setup');
+setupSelector.classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
 var randomArrayValue = function(myArray) {
   var rand = myArray[Math.floor(Math.random() * myArray.length)];
   return rand;
 }
 
-var firstCharacter = {
-  name = randomArrayValue(names),
-  coatColor = randomArrayValue(coatColors),
-  eyesColor = randomArrayValue(eyesColors),
+var characters = [
+  {
+    wizardNumber = 1,
+    name = randomArrayValue(names),
+    coatColor = randomArrayValue(coatColors),
+    eyesColor = randomArrayValue(eyesColors),
+  },
+  {
+    wizardNumber = 2,
+    name = randomArrayValue(names),
+    coatColor = randomArrayValue(coatColors),
+    eyesColor = randomArrayValue(eyesColors),
+  },
+  {
+    wizardNumber = 3,
+    name = randomArrayValue(names),
+    coatColor = randomArrayValue(coatColors),
+    eyesColor = randomArrayValue(eyesColors),
+  },
+  {
+    wizardNumber = 4,
+    name = randomArrayValue(names),
+    coatColor = randomArrayValue(coatColors),
+    eyesColor = randomArrayValue(eyesColors),
+  }
+];
+
+var renderWizard = function(wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = randomArrayValue(names);
+  wizardElement.querySelector('.wizard-coat').setAttribute('fill', randomArrayValue(coatColors));
+  wizardElement.querySelector('.wizard-eyes').setAttribute('fill', randomArrayValue(eyesColors));
+
+  return wizardElement;
 }
 
-var secondCharacter = {
-  name = randomArrayValue(names),
-  coatColor = randomArrayValue(coatColors),
-  eyesColor = randomArrayValue(eyesColors),
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < characters.length; i++) {
+  fragment.appendChild(renderWizard(characters));
 }
 
-var thirdCharacter = {
-  name = randomArrayValue(names),
-  coatColor = randomArrayValue(coatColors),
-  eyesColor = randomArrayValue(eyesColors),
-}
+similarListElement.appendChild(fragment);
 
-var fourthCharacter = {
-  name = randomArrayValue(names),
-  coatColor = randomArrayValue(coatColors),
-  eyesColor = randomArrayValue(eyesColors),
-}
-
+setupSelector.querySelector('.setup-similar').classList.remove('hidden');
