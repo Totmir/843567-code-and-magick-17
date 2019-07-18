effb50dadfac0eab0d9869a3f70909bb964d0c7e
totmir <alex.khokhlov1@ya.ru>
2019-07-18 16:28:25 +0300

    Fixed with mentor`s review

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,10 @@
-var names = ['Иван', 'Хуан' + 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = ['да' + 'Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var setupSelector = document.querySelectorAll('.setup');
-setupSelector.classList.remove('hidden');
+var setupsSelector = document.querySelectorAll('.setup');
+setupsSelector.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -12,35 +12,31 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var randomArrayValue = function(myArray) {
-  var rand = myArray[Math.floor(Math.random() * myArray.length)];
-  return rand;
+var getRandomArrayValue = function(myArray) {
+  return rand = myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 var characters = [
   {
-    wizardNumber = 1,
-    name = randomArrayValue(names),
-    coatColor = randomArrayValue(coatColors),
-    eyesColor = randomArrayValue(eyesColors),
+    name: randomArrayValue(names),
+    coatColor: randomArrayValue(coatColors),
+    eyesColor: randomArrayValue(eyesColors),
   },
   {
-    wizardNumber = 2,
-    name = randomArrayValue(names),
-    coatColor = randomArrayValue(coatColors),
-    eyesColor = randomArrayValue(eyesColors),
+    name: randomArrayValue(names),
+    coatColor: randomArrayValue(coatColors),
+    eyesColor: randomArrayValue(eyesColors),
   },
   {
-    wizardNumber = 3,
-    name = randomArrayValue(names),
-    coatColor = randomArrayValue(coatColors),
-    eyesColor = randomArrayValue(eyesColors),
+    wizardNumber: 3,
+    name: randomArrayValue(names),
+    coatColor: randomArrayValue(coatColors),
+    eyesColor: randomArrayValue(eyesColors),
   },
   {
-    wizardNumber = 4,
-    name = randomArrayValue(names),
-    coatColor = randomArrayValue(coatColors),
-    eyesColor = randomArrayValue(eyesColors),
+    name: randomArrayValue(names),
+    coatColor: randomArrayValue(coatColors),
+    eyesColor: randomArrayValue(eyesColors),
   }
 ];
 
@@ -56,9 +52,7 @@ var renderWizard = function(wizard) {
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < characters.length; i++) {
-  fragment.appendChild(renderWizard(characters));
+  fragment.appendChild(renderWizard(characters.length));
 }
 
 similarListElement.appendChild(fragment);
-
-setupSelector.querySelector('.setup-similar').classList.remove('hidden');
